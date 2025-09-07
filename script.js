e1ca71e761906787fa7ae8cc3af9991f24f50171
Mamun <majanfermun@gmail.com>
2025-09-07 15:53:33 +0600

    copy function done

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-
+//heart count
 const heart=document.getElementsByClassName("heart")
-console.log(heart)
+
 for (const element of heart) {
    element.addEventListener("click",function(){
         const heartCount=parseInt(document.getElementById("totalHeart").innerText);
         const totalHeartCount=heartCount+1;
-        console.log(totalHeartCount)
+        
         document.getElementById("totalHeart").innerText=totalHeartCount;
    })
     
 
 }
+
+//copy count
+
+const copies=document.getElementsByClassName("copy");
+
+
+
+for (const copy of copies) {
+    copy.addEventListener("click",function(){
+        const copyCount=parseInt(document.getElementById("totalCopy").innerText);
+        const totalCopyCount=copyCount+1;
+        
+        document.getElementById("totalCopy").innerText=totalCopyCount;
+
+        const card=copy.closest(".card");
+        const sourceEl = card.querySelector('.copy-source');  
+        const text=sourceEl.innerText;
+        navigator.clipboard.writeText(text)
+    })
+}
+
+
+
+// call
 
 
